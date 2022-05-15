2b40100aa60f83dd2fb509385a2d3f8db511fa8b
yura40klg <102523623+yura40klg@users.noreply.github.com>
2022-05-15 14:00:56 +0300

    Update hw.tf

--- hw.tf
+++ hw.tf
@@ -65,7 +65,7 @@ provisioner "remote-exec" {
       type        = "ssh"
       user        = "yura"
       private_key = "${file(var.ssh_key_private)}"
-      host = "external_ip_address_vm_2"
+      host = "${self.public_ip}"
     }
   }
 }
