729f65c6806e9eda85640406186f30a2c0c67cfa
yura40klg <102523623+yura40klg@users.noreply.github.com>
2022-05-15 11:27:38 +0300

    Update hw.tf

--- hw.tf
+++ hw.tf
@@ -83,10 +83,3 @@ output "internal_ip_address_vm_2" {
 }
 
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
-}
