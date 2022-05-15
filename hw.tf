3376b561bfe0ae6d0d9e71cbcf39c00597d15254
yura40klg <102523623+yura40klg@users.noreply.github.com>
2022-05-15 11:52:35 +0300

    Update hw.tf

--- hw.tf
+++ hw.tf
@@ -60,17 +60,14 @@ resource "yandex_compute_instance" "vm-2" {
   }
 }
 
-
-
-
-resource "yandex_vpc_network" "network-2" {
-  name = "network2"
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
 }
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-2.id
+  network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
@@ -80,6 +77,15 @@ output "internal_ip_address_vm_1" {
 
 output "internal_ip_address_vm_2" {
   value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
 
 
